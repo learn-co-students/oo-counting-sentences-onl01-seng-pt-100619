efe2560495da0c94d80fa1a7b52b6f49a6da9607
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-10-30 01:24:45 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
+    # binding.pry
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    # self.split.count
+    # self.gsub(/[[:punct:]]/, '').split
+    self.scan(/[^\!.?]+[\!.?]/).count
+    # binding.pry
   end
 end
